2ef14a1a5b8c610cdb3297073eeba3461a793ca2
PieMenard <pinedo.m.gustavo@gmail.com>
2025-07-03 10:39:39 -0400

    handle fullscreen

--- script.js
+++ script.js
@@ -38,6 +38,25 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+window.addEventListener('dblclick', () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement; //webkit is for safari
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
+
 // Scene
 const scene = new THREE.Scene();
 
