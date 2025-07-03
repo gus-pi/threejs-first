dfa968e9d88e3c65bad50a17cbe23ba7f7fb12d1
PieMenard <pinedo.m.gustavo@gmail.com>
2025-07-03 10:33:04 -0400

    Handle pixel ratio

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ window.addEventListener('resize', () => {
 
   //Update renderer
   renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
 // Scene
@@ -83,6 +84,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // Animate
 const clock = new THREE.Clock();
