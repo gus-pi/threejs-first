f74cd15787487b24febb59408f749ac38b05d2a7
PieMenard <pinedo.m.gustavo@gmail.com>
2025-07-04 01:01:15 -0400

    add debug ui

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import gsap from 'gsap';
+import GUI from 'lil-gui';
 
-//Cursor
-const cursor = {
-  x: 0,
-  y: 0,
-};
-window.addEventListener('mousemove', (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = -(event.clientY / sizes.height - 0.5);
+/**
+ * Debug
+ */
+const gui = new GUI({
+  width: 300,
+  title: 'Nice debug UI',
+  closeFolders: false,
 });
+// gui.close()
+// gui.hide()
+window.addEventListener('keydown', (event) => {
+  if (event.key == 'h') gui.show(gui._hidden);
+});
+
+const debugObject = {};
 
 /**
  * Base
@@ -17,126 +25,117 @@ window.addEventListener('mousemove', (event) => {
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
-// Sizes
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Object
+ */
+debugObject.color = '#a778d8';
+
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const material = new THREE.MeshBasicMaterial({
+  color: '#a778d8',
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const cubeTweaks = gui.addFolder('Awesome cube');
+// cubeTweaks.close()
+
+cubeTweaks.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation');
+
+cubeTweaks.add(mesh, 'visible');
+
+cubeTweaks.add(material, 'wireframe');
+
+cubeTweaks.addColor(debugObject, 'color').onChange(() => {
+  material.color.set(debugObject.color);
+});
+
+debugObject.spin = () => {
+  gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 });
+};
+cubeTweaks.add(debugObject, 'spin');
+
+debugObject.subdivision = 2;
+cubeTweaks
+  .add(debugObject, 'subdivision')
+  .min(1)
+  .max(20)
+  .step(1)
+  .onFinishChange(() => {
+    mesh.geometry.dispose();
+    mesh.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      debugObject.subdivision,
+      debugObject.subdivision,
+      debugObject.subdivision
+    );
+  });
+
+/**
+ * Sizes
+ */
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
 
-//Handle canvas resizing
 window.addEventListener('resize', () => {
-  //Update sizes
+  // Update sizes
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
-  //Update camera
+  // Update camera
   camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
 
-  //Update renderer
+  // Update renderer
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-window.addEventListener('dblclick', () => {
-  const fullscreenElement =
-    document.fullscreenElement || document.webkitFullscreenElement; //webkit is for safari
-
-  if (!fullscreenElement) {
-    if (canvas.requestFullscreen) {
-      canvas.requestFullscreen();
-    } else if (canvas.webkitRequestFullscreen) {
-      canvas.webkitRequestFullscreen();
-    }
-  } else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    }
-  }
-});
-
-// Scene
-const scene = new THREE.Scene();
-
-// Object
-// const mesh = new THREE.Mesh(
-//   new THREE.BoxGeometry(1, 1, 1),
-//   new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
-// );
-// scene.add(mesh);
-
-//Custom geometry
-
-const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
-
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
-
-const geometry = new THREE.BufferGeometry();
-geometry.setAttribute('position', positionsAttribute);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  wireframe: true,
-});
-
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-// Camera
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
   100
 );
-
-//Ortographic camera
-// const aspectRatio = sizes.width / sizes.height;
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspectRatio,
-//   1 * aspectRatio,
-//   1,
-//   -1,
-//   0.1,
-//   100
-// );
-
-// camera.position.x = 2;
-// camera.position.y = 2;
-camera.position.z = 3;
-camera.lookAt(mesh.position);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
 scene.add(camera);
 
-//Controls
+// Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-// controls.target.y = 1;
-// controls.update();
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  // Update objects
-  // mesh.rotation.y = elapsedTime;
-
-  //Update Camera
-  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  // camera.position.y = cursor.y * 3;
-  // camera.lookAt(mesh.position);
-
-  //Update controls
+  // Update controls
   controls.update();
 
   // Render
